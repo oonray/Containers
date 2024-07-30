f065df313a886a68cc059f1a7330c512d4161b99
oonray <alexanderbjornsrud@gmail.com>
2024-07-30 20:45:22 +0200

    .

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+wg genkey | tee /root/privatekey | wg pubkey > /root/publickey
+wg set wg0 listen-port 51820 private-key /root/privatekey peer $2 allowed-ips $1
+ip link set up dev wg0
 openvpn /root/.vpn.opvn
 service danted start
