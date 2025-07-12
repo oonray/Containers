04155a9ab9092f6d771472fec81d61d0f1a46119
oonray <alexanderbjornsrud@gmail.com>
2025-07-12 14:52:06 +0200

    containers change

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-wg genkey | tee /root/privatekey | wg pubkey > /root/publickey
-wg set wg0 listen-port 51820 private-key /root/privatekey peer $2 allowed-ips $1
-ip link set up dev wg0
-openvpn /root/.vpn.opvn
-service danted start
+#!/usr/bin/dumb-init /bin/bash
+service start danted
+tmux new &
