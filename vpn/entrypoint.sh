ed11071c119f5cd559a21edbac3ba0025e6ae61f
oonray <alexanderbjornsrud@gmail.com>
2025-07-18 11:15:36 +0200

    .

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/dumb-init /bin/bash
-sysctl -w net.ipv4.ip_forward=1
 cp /opt/dante/proxy_danted.conf /etc/danted.conf
-[ -s "/opt/vpn/vpn.ovpn" ] cp /opt/dante/vpn_danted.conf /etc/danted.conf
 
-[ -s "/etc/danted.conf" ] service start danted
-[ -s "/opt/vpn/vpn.ovpn" ] openvpn /opt/vpn/vpn.ovpn #&
+[ -s "/opt/vpn/vpn.ovpn" ] && cp /opt/dante/vpn_danted.conf /etc/danted.conf
+[ -s "/etc/danted.conf" ] && service start danted
+[ -s "/opt/vpn/vpn.ovpn" ] && openvpn /opt/vpn/vpn.ovpn #&
 #disown -r
