f90fae6878d4b7324a8086d23223cb2fa0365952
oonray <alexanderbjornsrud@gmail.com>
2025-07-18 11:52:55 +0200

    .

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/dumb-init /bin/bash
 cp /opt/dante/proxy_danted.conf /etc/danted.conf
 
-[ -s "/opt/vpn/vpn.ovpn" ] && cp /opt/dante/vpn_danted.conf /etc/danted.conf
-[ -s "/etc/danted.conf" ] && service start danted
+export DCONF=/opt/dante/proxy_danted.conf
+export DWORKERS=3
+export DPID=/run/dante.pid
+
+[ -s "/opt/vpn/vpn.ovpn" ] && export DCONF=/opt/dante/vpn_danted.conf
+[ -s "/etc/danted.conf" ] && sockd -f $DCONF -p $DPID -N $DWORKERS
 [ -s "/opt/vpn/vpn.ovpn" ] && openvpn /opt/vpn/vpn.ovpn #&
 #disown -r
