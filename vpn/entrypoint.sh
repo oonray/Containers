e71d421e71cc44a38e116735af7882e38662c0b1
oonray <alexanderbjornsrud@gmail.com>
2025-07-24 12:29:26 +0200

    .

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/dumb-init /bin/bash
 set -x
 
-export VPN_DEV=$(cat /opt/vpn/| grep dev | awk '{print $2}')
-export DANTED_CONF=/etc/danted/danted.conf
+export VPN_DEV=$(cat $VPNF | grep dev | awk '{print $2}')
+
+cat $DANCFG | tee $DANTECFG
 
 if [ -s $VPNF ]; then
-    openvpn $VPNF --dev "${VPN_DEV}0" || True &
-    cat $DANCFG | sed 's/external:.*/external: ${VPN_DEV}0/g' | tee $DANTED_CONF || True
-else
-    cp $DANCFG | tee $DANTED_CONF || True
+    /usr/sbin/openvpn $VPNF --dev "${VPN_DEV}0" || true &
+    cat $DANCFG | sed 's/external:.*/external: ${VPN_DEV}0/g' | tee $DANTECFG
 fi
-sockd -f $DANTED_CONF -p $DPID -N $DWORKERS || True &
+
+service danted start || true
 disown -r
