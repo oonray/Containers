389d94a3b970cad5116f76920be91067fbf33013
oonray <alexanderbjornsrud@gmail.com>
2025-08-02 19:30:52 +0200

    .

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -6,8 +6,8 @@ export VPN_DEV=$(cat $VPNF | grep dev | awk '{print $2}')
 cat $DANCFG | tee $DANTECFG
 
 if [ -s $VPNF ]; then
-    /usr/sbin/openvpn $VPNF --dev "${VPN_DEV}0" || true &
-    cat $DANCFG | sed 's/external:.*/external: ${VPN_DEV}0/g' | tee $DANTECFG
+    /usr/sbin/openvpn "${VPN_DEV}0" $VPNF  || true &
+    cat $DANCFG | sed "s/external:.*/external: ${VPN_DEV}0/g" | tee $DANTECFG
 fi
 
 service danted start || true
