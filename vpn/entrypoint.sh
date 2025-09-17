6d257376a60a5bbbf0cc2150ab52f978b55f80a0
alexander <oonray>
2025-09-17 09:15:16 +0000

    .

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/dumb-init /bin/bash
 set -x
 
-if [ -s $VPN_F ]; then
-    /usr/sbin/openvpn --config $VPN_F &
-else
-    echo "no vpn.ovpn in ${VPN_DIR}"
-fi
-
 if [ -s $DANTE_CONF ]; then
     service danted start
 else
     echo "${DANTE_CONF} not found"
 fi
 
-sleep infinity
+if [ -s $VPN_F ]; then
+    /usr/sbin/openvpn --config $VPN_F &
+else
+    echo "no vpn.ovpn in ${VPN_DIR}"
+    sleep infinity
+fi
