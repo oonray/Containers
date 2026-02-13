47e80f40060717cbf7e62deeb529c9de3b52b55f
oonray <alexanderbjornsrud@gmail.com>
2026-02-13 12:55:24 +0100

    .

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/dumb-init /usr/bin/zsh
 ssh-import-id gh:oonray
 service start sshd
-nvim --listen $L_HOST:$L_PORT --headless /home/$USR/.git
+#nvim --listen $L_HOST:$L_PORT --headless /home/$USR/.git
