343f67282146a7b337ca4d7ba6d27d7c37287892
oonray <alexanderbjornsrud@gmail.com>
2026-02-17 15:58:06 +0100

    .

--- dev/nvim-config/lua/config/treesitter.lua
+++ dev/nvim-config/lua/config/treesitter.lua
@@ -1,5 +1,4 @@
 local libs = {
-        path = Basepath .. "/.treesitter/",
         names = {
         "c","c_sharp","lua","vim","vimdoc","query","markdown",
         "markdown_inline","arduino","asm","bash","make","cpp","php",
@@ -8,27 +7,16 @@ local libs = {
 
 }
 
-function libs:dir()
-  if not vim.loop.fs_stat(self.path) then
-    vim.fn.system({
-        [[mkdir]],
-        [[-p]],
-        self.path})
-    end
-end
-
 function libs:setup()
-    self:dir()
-
-    vim.opt.rtp:append("," .. self.path)
-    vim.opt.runtimepath:append("," .. self.path)
-
     require('nvim-treesitter').setup {
         ensure_installed = self.names,
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
         fold = { enable = true },
-        install_dir = self.path,
     }
 end
+
+libs:setup()
+
+return libs
