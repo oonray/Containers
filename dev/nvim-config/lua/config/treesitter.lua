37a332f6b25c16c6fcb041cb249ffa6609d1a1bf
oonray <alexanderbjornsrud@gmail.com>
2026-02-17 15:50:21 +0100

    .

--- dev/nvim-config/lua/config/treesitter.lua
+++ dev/nvim-config/lua/config/treesitter.lua
@@ -1,15 +1,34 @@
 local libs = {
+        path = Basepath .. "/.treesitter/",
+        names = {
         "c","c_sharp","lua","vim","vimdoc","query","markdown",
         "markdown_inline","arduino","asm","bash","make","cpp","php",
         "go","http","jinja","javascript","jq","llvm","python",
         "ssh_config","tmux","typescript","yaml"}
 
-
-require('nvim-treesitter').setup {
-    ensure_installed = libs,
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-    fold = { enable = true },
-    install_dir = Basepath .. "/.treesitter/",
 }
+
+function libs:dir()
+  if not vim.loop.fs_stat(self.path) then
+    vim.fn.system({
+        [[mkdir]],
+        [[-p]],
+        self.path})
+    end
+end
+
+function libs:setup()
+    self:dir()
+
+    vim.opt.rtp:append("," .. self.path)
+    vim.opt.runtimepath:append("," .. self.path)
+
+    require('nvim-treesitter').setup {
+        ensure_installed = self.names,
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        fold = { enable = true },
+        install_dir = self.path,
+    }
+end
--- dev/nvim-config/lua/config/works.lua
+++ dev/nvim-config/lua/config/works.lua
@@ -1,30 +1,36 @@
 local works = {
-    path =  Basepath .. "/workspaces",
-    mru_sort = true,
-    auto_open = true,
-    auto_dir = true,
-    hooks = {
-        open_pre = {
-          "SessionsStop",
-          "silent %bd!",
-        },
-        open = function()
-            require("sessions").load(nil, {silent=true})
-        end,
+    folder =  Basepath .. "/.workspaces/",
+    conf = {
+        path =  Basepath .. "/.workspaces/workspaces.nvim",
+        mru_sort = true,
+        auto_open = true,
+        auto_dir = true,
+        hooks = {
+            open_pre = {
+              "SessionsStop",
+              "silent %bd!",
+            },
+            open = function()
+                require("sessions").load(nil, {silent=true})
+            end,
+        }
     }
 }
 
 function works:dir()
+  if not vim.loop.fs_stat(self.folder)
+  then
     vim.fn.system({
         [[mkdir]],
         [[-p]],
-        self.path})
+        self.folder})
+  end
 end
 
 function works:load()
     self:dir()
     require("telescope").load_extension("workspaces")
-    require("workspaces").setup(self)
+    require("workspaces").setup(self.conf)
 end
 
 return works
