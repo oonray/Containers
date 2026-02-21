69397729c33f4bcdaf4fef99464c19704459e3f6
oonray <alexanderbjornsrud@gmail.com>
2026-02-21 13:06:16 +0100

    .

--- dev/nvim-config/lua/config/autocmd.lua
+++ dev/nvim-config/lua/config/autocmd.lua
@@ -10,11 +10,37 @@ function auto:load()
             }
         )
         vim.api.nvim_create_autocmd(
-            {'BufEnter'}, {
-            pattern = {"*.h","*.c"},
+            {'BufEnter','BufRead'}, {
+            pattern = {"*.h"},
             callback = function(ev)
                 vim.opt.filetype = 'c'
                 vim.treesitter.start(ev.buf,'c')
+            end
+            }
+        )
+        vim.api.nvim_create_autocmd(
+            {'BufEnter','BufRead','BufNew'}, {
+            pattern = {"*.c"},
+            callback = function(ev)
+                vim.opt.filetype = 'c'
+                vim.treesitter.start(ev.buf)
+            end
+            }
+        )
+        vim.api.nvim_create_autocmd(
+            {'BufEnter','BufRead','BufNew'}, {
+            pattern = {"*.c"},
+            callback = function(ev)
+                vim.opt.filetype = 'c'
+                vim.treesitter.start(ev.buf)
+            end
+            }
+        )
+        vim.api.nvim_create_autocmd(
+            {'BufUnload','BufDelete'}, {
+            pattern = {"*.c"},
+            callback = function(ev)
+                vim.treesitter.stop(ev.buf)
             end
             }
         )
