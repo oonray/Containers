aaea894933069f6796936963ecf2c40e87a0293b
oonray <alexanderbjornsrud@gmail.com>
2026-02-23 23:10:25 +0100

    .

---
+++ comfy/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/dumb-init
+source ~/comfy/bin/activate
+cd ./ComfyUI \
+    && python3 \
+        main.py \
+        --use-sage-attention \
+        --listen 0.0.0.0 \
+        --port 8080 \
+        --disable-auto-launch \
+        --force-fp16 \
+        --preview-method none \
+        --cache-classic \
+        --enable-manager \
+        --normalvram \
+        --force-non-blocking \
+        --disable-smart-memory
